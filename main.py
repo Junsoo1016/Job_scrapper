0f0392211fa463c6a6caf88d6d1e1a1c29dca518
Jun <junsoopark92@gmail.com>
2022-08-03 06:37:21 +0900

    recap

--- main.py
+++ main.py
@@ -1,35 +1,14 @@
-from requests import get
-from bs4 import BeautifulSoup
+from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
 
-base_url = "https://kr.indeed.com/jobs?q="
-search_term = "python"
+keyword = input("What do you want to search for?")
 
-response = get(f"{base_url}{search_term}")
+indeed = extract_indeed_jobs(keyword)
+wwr = extract_wwr_jobs(keyword)
 
-if response.status_code != 200:
-    print("Can't request page")
-else:
-    results = []
-    soup = BeautifulSoup(response.text, 'html.parser')
-    job_list = soup.find("ul", class_="jobsearch-ResultsList")
-    jobs = job_list.find_all('li', recursive=False)
-    for job in jobs:
-        zone = job.find('div', class_='mosaic-zone')
-        if zone == None:
-            h2 = job.find('h2', class_="jobTitle")
-            anchor = job.select_one("h2 a")
-            title = anchor['aria-label']
-            link = anchor['href']
-            company = job.find("span", class_='companyName')
-            location = job.find('div', class_='companyLocation')
-            job_data = {
-                'link': f'https://kr.indeed.com{link}',
-                'company': company.string,
-                'location': location.string,
-                'position': title
-            }
-            print(job_data)
-            results.append(job_data)
-    for result in results:
-        print(result)
+jobs = indeed + wwr
+
+for job in jobs:
+    print(job)
+    print("///////////////")
+

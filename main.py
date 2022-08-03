3ba9bc66a09de1a54856e2d52e62dfabbb7897f7
Jun <junsoopark92@gmail.com>
2022-08-03 10:39:30 +0900

    finish job_scrapper

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ keyword = input("What do you want to search for?")
 
 indeed = extract_indeed_jobs(keyword)
 wwr = extract_wwr_jobs(keyword)
-
 jobs = indeed + wwr
 
+file = open(f"{keyword}.csv", "w")
+file.write("Position, Company, Location, URL\n")
+
 for job in jobs:
-    print(job)
-    print("///////////////")
+    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
+
+file.close()
+
+
+
 
